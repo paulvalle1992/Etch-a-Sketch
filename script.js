d1828eefd6f3dc75892c98c7570844c7ddc5b848
Paul V <paulvalle1992@gmail.com>
2023-03-14 11:46:47 -0700

    had grid size, grid displayes value, and range value all synced up visually and in the code as well.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.body.style.backgroundColor = "#CCFFFF"
 const etchASketch = document.getElementById('etchASketch')
-etchASketch.style.cssText = 'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); width: 480px; height: 480px;';
+etchASketch.style.cssText = 'display: grid; grid-template-columns: repeat(37, 1fr); grid-template-rows: repeat(37, 1fr); width: 480px; height: 480px;';
 
-for (i = 0; i < 256; i++) {
+
+let currentValue = document.getElementById('currentValue');
+let range = document.getElementById('pixelRange');
+currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
+range.addEventListener('change', () => {
+    currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
+});
+
+for (i = 0; i < (range.valueAsNumber * range.valueAsNumber); i++) {
+    
     let gridItem = document.createElement('div');
     gridItem.setAttribute('id', `gridItem${i}`);
     etchASketch.appendChild(gridItem);
-    gridItem.style.cssText = 'border: 1px solid black; background-color: white;'
-    
+    gridItem.style.cssText = 'border: 1px solid black; background-color: white;';
+
 }
-
-let currentValue = document.getElementById('currentValue');
-        let range = document.getElementById('pixelRange');
-        currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
-        range.addEventListener('change', () => {
-            currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
-        })
-
 
