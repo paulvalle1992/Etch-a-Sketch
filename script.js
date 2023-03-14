f40b0f8b6b5f6e1aa9b5380334643c9ccf870c59
Paul V <paulvalle1992@gmail.com>
2023-03-14 16:20:30 -0700

    added functionality to the actual etch a sketch, it now changes value in sync with range and grid size counter

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 document.body.style.backgroundColor = "#CCFFFF"
 const etchASketch = document.getElementById('etchASketch')
 let range = document.getElementById('pixelRange');
-etchASketch.style.cssText = 'display: grid; grid-template-columns: repeat(16, 1fr); width: 480px; height: 480px;';
-
-
+etchASketch.style.cssText = `display: grid; grid-template-columns: repeat(${range.valueAsNumber}, 1fr); width: 640px; height: 640px; background-color: white;`;
 let currentValue = document.getElementById('currentValue');
 currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
-range.addEventListener('change', () => {
+
+
+let rangeChange = range.addEventListener('change', () => {
     currentValue.textContent = `${range.valueAsNumber} x ${range.valueAsNumber}`;
 });
 
 for (i = 0; i < (range.valueAsNumber * range.valueAsNumber); i++) {
-    
     let gridItem = document.createElement('div');
-    gridItem.setAttribute('id', `gridItem${i}`);
+    gridItem.setAttribute('id', `gridItem gridItem${i}`);
     etchASketch.appendChild(gridItem);
-    gridItem.style.cssText = 'border: 1px solid black; background-color: white;';
-
+    gridItem.style.cssText = 'border: 1px solid black';
 }
 
+
+    
+
+range.addEventListener('mouseup', gridAdd);
+
+function gridAdd(e) {
+    rangeChange;
+    let gridRemove = etchASketch.querySelectorAll('div');
+    for (var i = 0; i < gridRemove.length; i++) {
+        gridRemove[i].remove();
+    }
+    etchASketch.style.cssText = `display: grid; grid-template-columns: repeat(${range.valueAsNumber}, 1fr); width: 640px; height: 640px; background-color: white;`;
+    for (i = 0; i < (range.valueAsNumber * range.valueAsNumber); i++) {
+        let gridItem = document.createElement('div');
+        gridItem.setAttribute('id', `gridItem gridItem${i}`);
+        etchASketch.appendChild(gridItem);
+        gridItem.style.cssText = 'border: 1px solid black';
+    }
+}
